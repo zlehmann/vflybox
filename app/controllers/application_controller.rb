25df88c8866089a26ba198f503807725662e57f4
Zachary Lehmann <zlehmann@gmail.com>
2018-12-30 18:02:47 +0000

    Built application_controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secrete, "virtualflyboxsecrete11221"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+end
